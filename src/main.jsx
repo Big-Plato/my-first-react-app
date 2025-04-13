35bbc9a2dffdc7c3f81d577c652b77ecfda3dfc1
Big-Plato <luisoctavius.sc@gmail.com>
2025-04-13 12:40:13 -0300

    new function Greeting

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,35 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import Greeting from "./Greeting.jsx"
+
+function MyButton() {
+  return(
+    <button>
+      I am a button
+    </button>
+  )
+}
+
+const user = {
+  name: "Luis Octávio",
+  imageUrl: "https://i.imgur.com/yXOvdOSs.jpg",
+  imageSize: 90,
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
+    <Greeting />
+    <h1>{user.name}</h1>
+    <img
+      className="avatar"
+      src={user.imageUrl}
+      alt={'Foto de ' + user.name}
+      style={{
+        width: user.imageSize,
+        height: user.imageSize,
+      }}
+      />
+    < MyButton />
+  </StrictMode>
 )
