ef20d1abf7a2e4cfd37c1ab30758aeb4adac56fb
Big-Plato <luisoctavius.sc@gmail.com>
2025-04-13 16:21:59 -0300

    merge resolved

--- src/Greeting.jsx
+++ src/Greeting.jsx
@@ -1,11 +1,5 @@
-<<<<<<< HEAD
 function Greeting() {
     return <h1>&quot;I swear by my pretty floral bonnet, I will end you&quot;</h1>;
-=======
-
-function Greeting() {
-    return <h1>&quot;Hello, world!;</h1>
->>>>>>> 607e86cb6ff0f176885271143c721bed7d50db68
 }
 
 export default Greeting;
